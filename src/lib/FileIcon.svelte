0fbdd8a395585f4e8e8ef896e3f74ee39279a42b
joshbker <43449531+joshbker@users.noreply.github.com>
2023-12-27 07:07:17 +0000

    feat: mp4 thumbnail

--- src/lib/FileIcon.svelte
+++ src/lib/FileIcon.svelte
@@ -9,6 +9,9 @@
 
 {#if ["jpeg", "jpg", "png", "gif", "ico"].includes(extension)}
 <img src={buildAbsoluteUrl(file, data.slug)} class="min-h-[10rem] object-cover rounded-xl" alt="">
+{:else if ["mp4"].includes(extension)}
+<!-- svelte-ignore a11y-media-has-caption -->
+<video src="{buildAbsoluteUrl(file, data.slug)}#t=1825" class="min-h-[10rem] object-cover rounded-xl"></video>
 {:else if ["bmp", "tiff", "tif", "webp", "svg", "raw", "psd", "ai", "eps", "pdf", "tga", "pcx", "pnm", "pbm", "pgm", "ppm", "xcf", "cr2", "nef", "orf", "srw", "raf", "dng", "rw2", "arw", "rwl", "mrw", "srf", "sr2", "mef", "erf", "mpo", "heif", "heic", "avif", "flif", "jpe", "jfif", "jif", "jff", "jng", "jnx", "heic", "jp2", "j2c", "j2k", "jpf", "jpm"].includes(extension)}
 <svg class={classes} viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM64 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm152 32c5.3 0 10.2 2.6 13.2 6.9l88 128c3.4 4.9 3.7 11.3 1 16.5s-8.2 8.6-14.2 8.6H216 176 128 80c-5.8 0-11.1-3.1-13.9-8.1s-2.8-11.2 .2-16.1l48-80c2.9-4.8 8.1-7.8 13.7-7.8s10.8 2.9 13.7 7.8l12.8 21.4 48.3-70.2c3-4.3 7.9-6.9 13.2-6.9z"/></svg>
 {:else if ["js", "ts", "jsx", "tsx", "java", "c", "cpp", "py", "rb", "php", "swift", "go", "cs", "dart", "scala", "rust", "kotlin", "r", "lua", "perl", "html", "css"].includes(extension)}
